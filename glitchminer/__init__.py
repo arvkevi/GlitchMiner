2aecc883d91e9311d0e400287be19d7e67eed536
Zihui Wu <91685968+wooozihui@users.noreply.github.com>
2024-10-28 21:59:32 +0800

    Update __init__.py

--- glitchminer/__init__.py
+++ glitchminer/__init__.py
@@ -1,4 +1,4 @@
-from .miner import initialize_model_and_tokenizer, GlitchMiner
+from .miner import initialize_model_and_tokenizer, GlitchMiner, strictly_glitch_verification
 from . import llm_template
 from . import tokenfilter
-__all__ = ['initialize_model_and_tokenizer', 'GlitchMiner']
+__all__ = ['initialize_model_and_tokenizer', 'GlitchMiner', 'strictly_glitch_verification']
